5869976de113aa7bf7c82828124bc7ac226e5a51
ForkΨKILLET <fork_killet@outlook.com>
2024-11-05 03:28:35 +0800

    feat: remote assets

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,44 @@
-import { Context, Schema } from 'koishi'
+import { Context, z, Random, Quester } from 'koishi'
+import RemoteAssets from 'koishi-plugin-assets-remote'
 
-export const name = 'w-assets-remote'
+import { createHmac } from 'node:crypto'
 
-export interface Config {}
-
-export const Config: Schema<Config> = Schema.object({})
-
-export function apply(ctx: Context) {
-  // write your plugin here
+declare module 'koishi' {
+    interface Context {
+        assetsAlt: RemoteAltAssets
+    }
 }
+
+class RemoteAltAssets extends RemoteAssets {
+    static name = 'w-assets-remote'
+
+    constructor(ctx: Context, config: RemoteAltAssets.Config) {
+        super(ctx, config)
+        ctx.set('assetsAlt', this)
+    }
+
+    public async uploadFile(file: Blob | Buffer | string, name: string): Promise<string> {
+        const params: Record<string, string> = {}
+
+        if (this.config.secret) {
+            params.salt = Random.id()
+            params.sign = createHmac('sha1', this.config.secret).update(name + params.salt).digest('hex')
+        }
+
+        const formData = new FormData()
+        const fileBlob: Blob = file instanceof Blob ? file : new Blob([ file ])
+        formData.append('asset', fileBlob, name)
+
+        const path = await this.http.put('', formData, { params })
+
+        return path
+    }
+}
+
+namespace RemoteAltAssets {
+    export interface Config extends RemoteAssets.Config {}
+
+    export const Config: z<Config> = z(JSON.parse(JSON.stringify(RemoteAssets.Config)))
+}
+
+export default RemoteAltAssets
